8b00ef88d583af9503ca575e7a4e1726eb04407a
CassiusMiller <cassius.miller@sdgku.edu>
2025-03-01 14:46:06 -0800

    final

--- script/app.js
+++ script/app.js
@@ -20,11 +20,41 @@ function saveTask() {
   console.log(taskToSave);
   
   //save to server
-
+  $.ajax({
+    type: "POST",
+    url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+    data: JSON.stringify(taskToSave),
+    contentType:"application/json",
+    success: function (response) {
+      console.log(response);
+      
+    },
+    error: function (error) {
+      console.log(error);
+      
+    }
+  });
   //display the task
   displayTask(taskToSave);
     
 }
+
+function testRequest() {
+  $.ajax({
+    type: "GET",
+    url: "http://fsdiapi.azurewebsites.net",
+    success: function (response) {
+      console.log(response);
+      
+    },
+    error: function (error) {
+      console.log(error);
+      
+    }
+    
+  })
+}
+
 
 function displayTask(task) {
   let syntax = `
@@ -40,6 +70,30 @@ function displayTask(task) {
     </div>
     </div>`;
     $(".pendingTask").append(syntax);
+}
+
+function loadTask() {
+  $.ajax({
+    type: "GET",
+    url: "http://fsdiapi.azurewebsites.net/api/tasks",
+    success: function (response) {
+      console.log(response);
+      let data = JSON.parse(res);
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        let task = data[i];
+        if (task.name=== "Cassius") {
+          displayTask(task)
+        }
+      }
+      
+    },
+    error: function (error) {
+      console.log(error);
+      
+    }
+    
+  })
 }
 
 function init(){
--- script/task.js
+++ script/task.js
@@ -1,10 +1,11 @@
 class Task {
-  constructor(title, desription, color, startdate, status, budget) {
+  constructor(title, desription, color, startdate, status, budget, name) {
     this.title = title
     this.desription = desription
     this.color = color
     this.startdate = startdate
     this.status = status
     this.budget = budget
+    this.name = "Cassius"
   }
 }
